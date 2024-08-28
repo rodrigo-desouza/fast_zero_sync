20cc045db2c377813fdfbd4f5f98d202c213b190
Rodrigo Souza <rodrigo.souzr@gmail.com>
2024-08-27 21:45:06 -0300

    Aula 02 - Consturindo Schema de Message e Exercícios

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schema import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olar Mundo'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_root_html():
+    return """
+        <html>
+        <head>
+            <title> Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo </h1>
+        </body>
+        </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,23 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmando)
-    assert response.json() == {'message': 'Olar Mundo'} # Assert (Afirmando)
+    assert response.json() == {'message': 'Olar Mundo'}  # Assert (Afirmando)
+
+
+def test_read_root_deve_retornar_html():
+    client = TestClient(app)  # Arrange (organização)
+
+    response = client.get('/html')  # Act (ação)
+
+    html_esperado = """
+        <html>
+        <head>
+            <title> Nosso olá mundo!</title>
+        </head>
+        <body>
+            <h1> Olá Mundo </h1>
+        </body>
+        </html>"""
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirmando)
+    assert response.text == html_esperado  # Assert (Afirmando)
